c68e7f323466cf6a01965a377133ad9f824d5900
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2021-10-14 18:02:10 +0900

    Added configuration extension capabilities
    Both third and first party methods. You can make configuration extensions to add compatibility to your code for certain maps and gamemodes or make an entirely external addon with just extension files to modify an existing loader's configuration.

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -37,6 +37,9 @@ local config = {
 --what do we say we are when we load up?
 local branding = "Cryotheum's Preconfigured Loader"
 
+--path to the folder for merging entries into the config folder
+local loader_extension_path = true
+
 --maximum amount of folders it may go down in the config tree
 local max_depth = 4
 
@@ -56,6 +59,7 @@ local color_significant = Color(0, 255, 0)
 
 
 ----local variables, don't change
+	local active_gamemode = engine.ActiveGamemode()
 	local fl_bit_band = bit.band
 	local fl_bit_rshift = bit.rshift
 	local highest_priority = 0
@@ -67,6 +71,13 @@ local color_significant = Color(0, 255, 0)
 	}
 	
 	local load_function_shift = table.Count(load_functions)
+	
+	--directory stuff
+	loader_extension_path = loader_extension_path == true and "extensions/" or loader_extension_path
+	local loader_full_source = debug.getinfo(1, "S").short_src
+	local loader_path = string.sub(loader_full_source, select(2, string.find(loader_full_source, "lua/", 1, true)) + 1)
+	local loader_directory = string.GetPathFromFilename(loader_path)
+	local map = game.GetMap()
 
 --local functions
 local function construct_order(config_table, depth, path)
@@ -91,6 +102,38 @@ local function construct_order(config_table, depth, path)
 	end
 end
 
+local function find_extensions(file_list, root_directory, directory)
+	if file.Exists(root_directory .. directory, "LUA") then
+		local files = file.Find(root_directory .. directory .. "/*", "LUA")
+		
+		MsgC(color_generic, " Appending [" .. directory .. "] extension configurations...\n")
+		
+		for index, file_name in ipairs(files) do table.insert(file_list, directory .. "/" .. file_name) end
+	else MsgC(color_generic, "No [" .. directory .. "] extension configurations to append.\n") end
+end
+
+local function grab_extensions(extension_directory)
+	--grab all files in the extension folder, these are loaded first
+	local files = file.Find(extension_directory .. "*", "LUA")
+	
+	if files then
+		find_extensions(files, extension_directory, "gamemode/" .. active_gamemode)
+		find_extensions(files, extension_directory, "map/" .. map)
+		
+		--load all those files and merge their return if its a table
+		for index, file_name in ipairs(files) do
+			local file_path = extension_directory .. file_name
+			local config_extension, remove_from_download = include(file_path)
+			
+			if config_extension then table.Merge(config, config_extension) end
+			if remove_from_download or CLIENT then continue
+			else MsgC(color_generic, " ]    " .. file_name .. " included and added for download\n") end
+			
+			AddCSLuaFile(file_path)
+		end
+	else MsgC(color_significant, "No extension folder.\nThis is not an error, the path used for extensions does not exist most likely meaning all extensions are third party.\n") end
+end
+
 local function load_by_order(prefix_directory)
 	for priority = 0, highest_priority do
 		local script_paths = load_order[priority]
@@ -111,15 +154,21 @@ local function load_by_order(prefix_directory)
 end
 
 local function load_scripts(command_reload)
-	MsgC(color_generic, "\n\\\\\\ ", color_significant, branding, color_generic, " ///\n\n", color_significant, "Constructing load order...\n")
+	--load extensions if enabled; this will be converted into extensions/ if true
+	if loader_extension_path then
+		MsgC(color_generic, "\n\\\\\\ ", color_significant, branding, color_generic, " ///\n\n", color_significant, "Grabbing extension configurations...\n")
+		grab_extensions(loader_directory .. loader_extension_path)
+		MsgC(color_significant, "\nGrabbed extension configurations.\n\nConstructing load order...\n")
+	else MsgC(color_generic, "\n\\\\\\ ", color_significant, branding, color_generic, " ///\n\n", color_significant, "Constructing load order...\n") end
+	
+	--create a table of load priorities
 	construct_order(config, 1, "")
 	MsgC(color_significant, "\nConstructed load order.\n\nLoading scripts by load order...\n")
 	
+	--then load them in that order relative to a path
 	if command_reload then
-		local full_path = debug.getinfo(1, "S").short_src
-		
 		MsgC(color_generic, "\n!!! ", color_significant, "PRECONFIGURED LOADER NOTE", color_generic, " !!!\nAs the load was requested using command, the source file for the loader be used as a prefix for the following includes. You may experience issues with scripts loaded using this command that do not persist when the file is loaded by an include. If that happens, try making the file include itself instead of executing the load_scripts function in the reload command. You can do this by setting self_include_reload to true (attempts to automatically grab the file's path) or the full path of the loader.\n||| ", color_significant, "PRECONFIGURED LOADER NOTE", color_generic, " |||\n\n")
-		load_by_order(string.GetPathFromFilename(string.sub(full_path, select(2, string.find(full_path, "lua/", 1, true)) + 1)))
+		load_by_order(loader_directory)
 	else load_by_order("") end
 	
 	MsgC(color_significant, "\nLoaded scripts.\n\n", color_generic, "/// ", color_significant, "All scripts loaded.", color_generic, " \\\\\\\n\n")
@@ -129,12 +178,9 @@ end
 concommand.Add(reload_command, function(ply)
 	if CLIENT or not IsValid(ply) or ply:IsSuperAdmin() then
 		if self_include_reload then
-			if isstring(self_include_reload) then include(self_include_reload)
-			else
-				local full_path = debug.getinfo(1, "S").short_src
-				
-				include(string.sub(full_path, select(2, string.find(full_path, "lua/", 1, true)) + 1))
-			end
+			--zero timers to prevent breaking of autoload?
+			if isstring(self_include_reload) then timer.Simple(0, function() include(self_include_reload) end)
+			else timer.Simple(0, function() include(loader_path) end) end
 		else load_scripts(true) end
 	end
 end, nil, "Reload all " .. branding .. " scripts.")
